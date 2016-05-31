69f08c546d4e32fd7297cb6d64bf518ed934fbb3
Lee Zamparo <zamparo@gmail.com>
2016-05-31 19:33:16 -0400

    still need to finish model spec in Lasagne, and rest of TODO

--- src/models/basset_onehot.py
+++ src/models/basset_onehot.py
@@ -32,19 +32,61 @@ data_loader = load.DNaseDataLoader() # do I need to specify chunks, chunk size h
     #patch_size=patch_size, chunk_size=chunk_size, augmentation_params=augmentation_params,
     #augmentation_transforms_test=augmentation_transforms_test, validation_split_path=validation_split_path)
 
-
-# shortcut to Lasagne layers needed
-
+### The output of the basset model with fewer filters
+#(1): nn.SpatialConvolution(4 -> 150, 19x1) ** should be 300
+    #(2): nn.SpatialBatchNormalization
+    #(3): nn.ReLU
+    #(4): nn.SpatialMaxPooling(3,1,3,1)
+    #(5): nn.SpatialConvolution(150 -> 100, 11x1) ** should be 200
+    #(6): nn.SpatialBatchNormalization
+    #(7): nn.ReLU
+    #(8): nn.SpatialMaxPooling(4,1,4,1)
+    #(9): nn.SpatialConvolution(100 -> 100, 7x1) ** should be 200
+    #(10): nn.SpatialBatchNormalization
+    #(11): nn.ReLU
+    #(12): nn.SpatialMaxPooling(4,1,4,1)
+    #(13): nn.Reshape(1000)
+    #(14): nn.Linear(1000 -> 1000)
+    #(15): nn.BatchNormalization
+    #(16): nn.ReLU
+    #(17): nn.Dropout(0.300000)
+    #(18): nn.Linear(1000 -> 1000)
+    #(19): nn.BatchNormalization
+    #(20): nn.ReLU
+    #(21): nn.Dropout(0.300000)
+    #(22): nn.Linear(1000 -> 164)
+    #(23): nn.Sigmoid
+    
+    
+### The pretrained_params.txt file
+    #conv_filters    300
+    #conv_filters    200
+    #conv_filters    200
+    #conv_filter_sizes       19
+    #conv_filter_sizes       11
+    #conv_filter_sizes       7
+    #pool_width              3
+    #pool_width              4
+    #pool_width              4
+    #hidden_units            1000
+    #hidden_units            1000
+    #hidden_dropouts         0.3
+    #hidden_dropouts         0.3
+    #learning_rate           0.002
+    #weight_norm             7      *** called after each parameter update, during training.  See https://github.com/nicholas-leonard/dpnn#nn.Module.maxParamNorm for details, not sure if supported in Lasagne.
+    #momentum                0.98    
+    
+    # shortcut to Lasagne layers needed
+    
 Conv2DLayer = nn.layers.dnn.Conv2DDNNLayer
 MaxPool2DLayer = nn.layers.dnn.MaxPool2DDNNLayer
 BatchNormLayer = nn.layers.BatchNormLayer
-
 
 def build_model():
     # TODO: update for basset model via output script.  Output layer should be dense layer of 164, with softmax nonlinearity (I think, check this).
     l0 = nn.layers.InputLayer((batch_size, 1, data_rows, data_cols))
 
-    l1 = Conv2DLayer(l0c, num_filters=32, filter_size=(3, 3), border_mode="same", W=nn_plankton.Conv2DOrthogonal(1.0), b=nn.init.Constant(0.1), nonlinearity=nn_plankton.leaky_relu, untie_biases=True)
+    l1 = Conv2DLayer(l0c, num_filters=32, filter_size=(19, 1), border_mode="same", W=nn_plankton.Conv2DOrthogonal(1.0), b=nn.init.Constant(0.1), nonlinearity=nn_plankton.leaky_relu, untie_biases=True)
     l1b = Conv2DLayer(l1a, num_filters=16, filter_size=(3, 3), border_mode="same", W=nn_plankton.Conv2DOrthogonal(1.0), b=nn.init.Constant(0.1), nonlinearity=nn_plankton.leaky_relu, untie_biases=True)
     l1 = MaxPool2DLayer(l1b, ds=(3, 3), strides=(2, 2))
     l1r = dihedral_fast.CyclicConvRollLayer(l1)
