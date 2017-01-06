e26584507295dd45b3e78318c9a0eb131dae07d0
Lee Zamparo <zamparo@gmail.com>
2017-01-05 22:31:45 -0500

    encoding transfer should work, but still need to convert into a bed file

--- src/data_preparation/extract_hematopoetic_subset.py
+++ src/data_preparation/extract_hematopoetic_subset.py
@@ -4,12 +4,21 @@ import numpy as np
 
 ### Grab all cells within the hematopoetic lineage out of the Roadmap data used for Alvaro's paper
 
-os.chdir('/Users/zamparol/projects/SeqDemote/data/DNase')
+os.chdir(os.path.expanduser('~/projects/SeqDemote/data/DNase'))
 
 
 # read both peaks data file, DNase counts file
 peaks = pandas.read_csv("peaksTable.txt", sep="\t")
 counts = pandas.read_csv("DNaseCnts.txt", sep="\t")
+
+# establish a dictionary to return indicators in the form of an np.array
+pattern_dict = {}
+pattern_dict['H1hesc'] = np.asarray([1,0,0,0,0,0], dtype=int)
+pattern_dict['CD34'] = np.asarray([0,1,0,0,0,0], dtype=int)
+pattern_dict['CD14'] = np.asarray([0,0,1,0,0,0], dtype=int)
+pattern_dict['CD56'] = np.asarray([0,0,0,1,0,0], dtype=int)
+pattern_dict['CD3'] = np.asarray([0,0,0,0,1,0], dtype=int)
+pattern_dict['CD19'] = np.asarray([0,0,0,0,0,1], dtype=int)
 
 def parse_access_pattern(pattern):
     ''' The access pattern in Alvaro's data is a dash delimited string indicating in which cell-types this particular peak is accessible.
@@ -18,9 +27,10 @@ def parse_access_pattern(pattern):
         if pattern is CD34-CD14, output is np.asarray([0,1,1,0,0,0])
     
     '''
-    parts = pattern.split('-')
+    arrays = tuple([pattern_dict[d] for d in pattern.split('-')])
+    return np.sum(np.vstack(arrays), axis=0)
     
-
+        
 # produce Basset-style bed file for peaks, and activations text file for processing into a tensor-style data set
 
 
