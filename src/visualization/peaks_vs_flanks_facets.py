19aee579b8712eb66fba2a7214c2c6e1143590f2
Lee Zamparo <zamparo@gmail.com>
2017-01-31 17:49:15 -0500

    almost done refactoring, need to tweak validation AUC specific plot to include SeqGL comparisons

--- src/visualization/peaks_vs_flanks_facets.py
+++ src/visualization/peaks_vs_flanks_facets.py
@@ -1,6 +1,7 @@
 import os
 import ggplot as gg
-from plot_model_pkls import pkl_to_df
+import numpy as np
+from model_etl import pkl_to_df
 
 ### Peaks vs Flanks specific model validation figure: faceted plot of model AUC, training and validation losses.
 model_dir = "~/projects/SeqDemote/results/peaks_vs_flanks"
@@ -16,9 +17,13 @@ faceted_plot = gg.ggplot(df, gg.aes(x='epoch', y='score', color='model')) + \
 faceted_plot.save(os.path.join(os.path.expanduser('~/projects/SeqDemote/results/diagnostic_plots/'),title_suffix,'pvf_facet_plots.png'))
 # fig, ax = plt.subplots(3, 1, figsize=(3 * 7.5, 3 * 5))
 
-## Plot all models validation AUC
+## Plot all models validation AUC, along with SeqGL comparisons
 SeqGL_comparisons = [0.9067819,0.8348001,0.8497134,0.8832141,0.8635396,0.8622086]
 SeqGL_celltypes = ["CD14","CD19","CD3","CD34","CD56","H1hesc"]
+SeqGL_max = max(SeqGL_comparisons)
+SeqGL_min = min(SeqGL_comparisons)
+SeqGL_mean = np.asarray(SeqGL_comparisons).mean()
+
 validation_auc = gg.ggplot(df_auc, gg.aes(x='epoch', y='validation AUC', color='model')) + \
     gg.geom_line(size=2.0) + \
         gg.xlab('Epoch') + \
