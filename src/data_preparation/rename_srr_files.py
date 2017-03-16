361ca8df03a67dc9c4fb20c560ec82780f642dfb
Lee Zamparo <zamparo@gmail.com>
2017-03-16 13:16:49 -0400

    changed formatting again, to facilitate merging of tech reps

--- src/data_preparation/rename_srr_files.py
+++ src/data_preparation/rename_srr_files.py
@@ -47,7 +47,7 @@ def encode_prefix(filename, celltype, rep_number=None):
     donor = srr_to_donor_id[srr_id]
     if not rep_number:
         rep_number = donor_cell_type_rep_number[(donor, celltype)].pop()
-    new_name = celltype + ".rep" + str(rep_number) + "." + str(donor) + "_" + suffix 
+    new_name = celltype + "." + str(donor) + "-rep" + str(rep_number) + "_" + suffix 
     return new_name, rep_number
 
 # walk directories of data, renaming files as needed
