01a5129199f861885bdc384bbc749b820186c573
Lee Zamparo <zamparo@gmail.com>
2017-03-16 12:49:32 -0400

    rep number now persists within a directory, applied to both left and right read pairs

--- src/data_preparation/rename_srr_files.py
+++ src/data_preparation/rename_srr_files.py
@@ -40,15 +40,16 @@ for index, line in experiments.iterrows():
 for key in donor_cell_type_rep_number.keys():
     donor_cell_type_rep_number[key].sort(reverse=True)
 
-def encode_prefix(filename, celltype):
+def encode_prefix(filename, celltype, rep_number=None):
     ''' take a fastq file encoded by its SRR id, re-encode it as 
     <donor id>_rep?_<original suffix>'''
     pdb.set_trace()
     srr_id, suffix = filename.split("_")
     donor = srr_to_donor_id[srr_id]
-    rep_number = donor_cell_type_rep_number[(donor, celltype)].pop()
+    if not rep_number:
+        rep_number = donor_cell_type_rep_number[(donor, celltype)].pop()
     new_name = celltype + "_" + str(donor) + "_" + str(rep_number) + "_" + suffix 
-    return new_name
+    return new_name, rep_number
 
 # walk directories of data, renaming files as needed
 for dirname, dirs, files in os.walk('/cbio/cllab/nobackup/zamparol/heme_ATAC/data/fastq'):
@@ -56,7 +57,10 @@ for dirname, dirs, files in os.walk('/cbio/cllab/nobackup/zamparol/heme_ATAC/dat
         prefix, rep = os.path.split(dirname)
         _, celltype = os.path.split(prefix)
         fastq_files = [f for f in files if f.endswith("fastq.gz")]
-        renamed_files = [encode_prefix(f, celltype) for f in fastq_files]
+        rep_number = None
+        for f in fastq_files:
+            renamed_file, rep_number = encode_prefix(f, celltype, rep_number)
         pdb.set_trace()
+        rep_number = None
         print(dirname, " : ", ",".join(fastq_files))
         print(dirname, " : ", ",".join(renamed_files))
