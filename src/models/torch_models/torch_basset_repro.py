a04c74bd5e912e5dbfa797f3fe1abb86f2e2ee6d
Lee Zamparo <zamparo@gmail.com>
2018-06-01 10:32:37 -0400

    added Soumiths very generous implementation of untied biases conv2d

--- src/models/torch_models/torch_basset_repro.py
+++ src/models/torch_models/torch_basset_repro.py
@@ -4,6 +4,46 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.modules.utils import _pair
+
+class Conv2dUntiedBias(nn.Module):
+    def __init__(self, height, width, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1):
+        kernel_size = _pair(kernel_size)
+        stride = _pair(stride)
+        padding = _pair(padding)
+        dilation = _pair(dilation)
+
+        if in_channels % groups != 0:
+            raise ValueError('in_channels must be divisible by groups')
+        if out_channels % groups != 0:
+            raise ValueError('out_channels must be divisible by groups')
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.dilation = dilation
+        self.groups = groups
+        self.weight = Parameter(torch.Tensor(
+                out_channels, in_channels // groups, *kernel_size))
+        self.bias = Parameter(torch.Tensor(out_channels, height, width))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        n = self.in_channels
+        for k in self.kernel_size:
+            n *= k
+        stdv = 1. / math.sqrt(n)
+        self.weight.data.uniform_(-stdv, stdv)
+        self.bias.data.uniform_(-stdv, stdv)
+
+
+    def forward(self, input):
+        output = F.conv2d(input, self.weight, None, self.stride,
+                        self.padding, self.dilation, self.groups)
+        # add untied bias
+        output += self.bias.unsqueeze(0).repeat(input.size(0), 1, 1, 1)
+        
 
 from load_pytorch import DNase_Train_Dataset, DNase_Valid_Dataset
 
@@ -98,11 +138,29 @@ class BassetRepro(nn.Module):
     
     
 net = BassetRepro()
+
+
+def init_weights(m, gain=nn.init.calculate_gain('relu')):
+    ''' Recursively initalizes the weights of a network. '''
+    
+    if isinstance(m, nn.Linear):
+        torch.nn.init.xavier_uniform(m.weight)
+        m.bias.data.fill_(0.01)
+    
+    if isinstance(m, nn.Conv2d):
+        torch.nn.init.orthogonal(m.weight, gain)
+        m.bias.data.fill_(0.1)
+
+net.apply(init_weights)
+
 weights, biases = [], []
 for name, p in net.named_parameters():
     if 'bias' in name:
         biases += [p]
+        
     else:
         weights += [p]
-        
+  
+  
+# Initialize the params        
 optimizer = torch.optim.RMSprop
