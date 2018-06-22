7e43d803f2cd1c379ef05b653f13a66094253034
Lee Zamparo <zamparo@gmail.com>
2018-06-22 15:15:54 -0400

    added aupr average line for BindSpace comparison

--- src/visualization/BindSpace_models_facets.py
+++ src/visualization/BindSpace_models_facets.py
@@ -12,6 +12,8 @@ title_suffix = os.path.basename(model_dir)
 df_auroc = df[df['measure'] == "validation AUROC"]
 df_aupr = df[df['measure'] == "validation AUPR"]
 
+BindSpace_avg = 0.31415662596238375
+
 auroc_plot = gg.ggplot(df_auroc, gg.aes(x="epoch", y="score", color="model")) + \
     gg.geom_line(size=1.5) + \
     gg.xlab("Epoch") + \
@@ -25,5 +27,6 @@ aupr_plot = gg.ggplot(df_aupr, gg.aes(x="epoch", y="score", color="model")) + \
     gg.xlab("Epoch") + \
     gg.ggtitle("Validation Set AUPR") + \
     gg.ylab("AUPR") + \
+    gg.geom_hline(y=BindSpace_avg, size=0.5, color='black',linetype='dotted') + \
     gg.scale_x_continuous(breaks=[i for i in range(20)], labels=[i for i in range(20)])
 aupr_plot.save(os.path.join(os.path.expanduser("~/projects/SeqDemote/results/diagnostic_plots/BindSpace_embedding/"),"AUPR.png"), width=20)
