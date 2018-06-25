2956019ef8d4a732263907eaf85d9b5605ead193
Lee Zamparo <zamparo@gmail.com>
2018-06-25 00:11:12 -0400

    should be almost good to go

--- src/submit_scripts/validate_all_models.sh
+++ src/submit_scripts/validate_all_models.sh
@@ -1,9 +1,29 @@
 #! /bin/bash
 
-pushd ~/projects/SeqDemote/src/models/torch_models
-models=$(ls *.py)
-popd
-for model in $models
+pushd ~/projects/SeqDemote/src/models/torch_models/embedded_ATAC_models
+declare -a models
+for file in $(ls -1 *.py | sort -n)
 do
-	bsub -env "all, MODEL=$model" < run_model.lsf
+    models=("${models[@]}" "$file")
+done
+echo "Got all models, ${#models[@]}"
+popd
+
+pushd ~/projects/SeqDemote/results/BindSpace_embedding_extension
+declare -a savestates
+for file in $(ls -1 *.ptm | sort -n)
+do 
+   savestates=("${savestates[@]}" "$file")   
+done
+echo "Got all save states, ${#savestates[@]}"
+popd
+
+model_prefix="models/torch_models/embedded_ATAC_models"
+
+for (( i=0; i<${#models[@]}; i++ ));
+do
+	model=${models[$i]}
+        savestate=${savestates[$i]}
+	echo "-env all, MODEL=$model_prefix/$model, STATE=$savestate"
+	#bsub -env "all, MODEL=$model, STATE=$savestate" < validate_model.lsf
 done
