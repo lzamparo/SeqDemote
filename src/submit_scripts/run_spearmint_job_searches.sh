26cbd05304e8d7b2101721e626ccc9aa358c548c
Lee Zamparo <zamparo@gmail.com>
2018-07-05 12:21:27 -0400

    renamed submit scripts

--- src/submit_scripts/run_spearmint_job_searches.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-pushd ~/projects/SeqDemote/src/models/torch_models/embedded_ATAC_models
-models=$(ls *.py)
-popd
-for model in $models
-do
-	bsub -env "all, MODEL=$model" < run_model.lsf
-done
